2b0f2ac64f19469efc715684b9dcf6f293edeaf8
Vladislav1306 <70974645+Vladislav1306@users.noreply.github.com>
2021-11-06 10:47:01 +0300

    Update test_one.py

--- test_one.py
+++ test_one.py
@@ -3,7 +3,7 @@ def func(x):
     return x + 1
 
 def test_answer():
-    assert func(4) == 5
+    assert func(3) == 5
 
 def test_passing():
-    assert (1, 2, 3) == (1, 2, 3)
+    assert (1, 2, 4) == (1, 2, 3)
